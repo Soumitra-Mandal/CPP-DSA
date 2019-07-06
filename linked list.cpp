87de33ac1e6533313429ebbf94c6c4fdd7125c3a
Soumitra Mandal <soumitramandal1999@gmail.com>
2019-07-06 22:02:12 +0530

    linked list
    contains linked list, how to add elements

---
+++ linked list.cpp
@@ -0,0 +1,38 @@
+#include<iostream>
+using namespace std;
+struct node
+{int data;
+node *next;
+};
+class linked_list
+{
+	private:
+		node *head,*tail;
+	public:
+		linked_list()
+		{ head=NULL;
+		tail=NULL;
+		
+				}	
+		void add_node(int n)
+		{
+			node *temp= new node;
+			temp->data=n;
+			temp->next=NULL;
+			
+			if(head == NULL)
+			{ head=temp;
+			tail=temp;
+			}
+			else
+			{tail->next=temp;
+			tail=tail->next;
+			}
+		}	
+};
+int main()
+{linked_list a;
+a.add_node(1);
+a.add_node(2);
+return 0;
+}
